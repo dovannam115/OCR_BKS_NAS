451b1754d61432278817c629881ec465a42af96e
Do Van Nam <nam.dovan@gic.com.vn>
2024-12-06 16:10:02 +0700

    Add all files

---
+++ app.py
@@ -0,0 +1,103 @@
+# Import necessary libraries
+import streamlit as st
+import numpy as np
+import cv2
+from PIL import Image
+import tempfile
+from license_plate_detection import *
+
+# Define the main function
+def main():
+    # Set the title of the Streamlit app
+    st.title("License Plate Detection and OCR with YOLO-NAS")
+
+    # Create a selection box in the sidebar to choose detection mode (image or video)
+    app_mode = st.sidebar.selectbox(
+            'Choose Detection Mode', ['Run on Image', 'Run on Video']
+    )
+
+    # If the chosen mode is "Run on Image"
+    if app_mode == 'Run on Image':
+        # Create a slider to adjust confidence level
+        confidence = st.sidebar.slider(
+            'Confidence', min_value=0.0, max_value=1.0
+        )
+
+        # Create a file uploader for image files
+        img_file = st.sidebar.file_uploader(
+            "Upload an Image", type=["png", "jpg", "jpeg"]
+        )
+
+        # Default image for demonstration purposes
+        demo = r"./OCR-2/valid/images/11_jpg.rf.5d7b9e03b6e040fcb3c9e1d7c2a6b590.jpg"
+
+        # Load the image from the file uploader or use the default demo image
+        if img_file is not None:
+            img = cv2.imdecode(np.fromstring(
+                img_file.read(), np.uint8), 1
+            )
+            image = np.array(Image.open(img_file))
+        else:
+            img = cv2.imread(demo)
+            image = np.array(Image.open(demo))
+
+        # Display the original image in the sidebar
+        st.sidebar.text("Original Image")
+        st.sidebar.image(image)
+
+        # Perform object detection and OCR on the image
+        object_detection_ocr(img, confidence)
+
+    # If the chosen mode is "Run on Video"
+    elif app_mode == "Run on Video":
+        # Create a slider to adjust confidence level
+        confidence = st.sidebar.slider(
+            'Confidence', min_value=0.0, max_value=1.0
+        )
+
+        # Create a file uploader for video files
+        video = st.sidebar.file_uploader(
+            "Upload a Video", type=["mp4", "avi", "mov", "asf", "moov"]
+        )
+
+        # Default video for demonstration purposes
+        demo = r"./Videos/demo.mp4"
+
+        # Create a temporary file to store the uploaded video
+        tffile = tempfile.NamedTemporaryFile(suffix='.mp4', delete=False)
+
+        # If no video is uploaded, display the demo video in the sidebar
+        if not video:
+            video_stream = cv2.VideoCapture(video)
+            tffile.name = demo
+            demo_vid = open(tffile.name, 'rb')
+            demo_bytes = demo_vid.read()
+            st.sidebar.text("Input Video")
+            st.sidebar.video(demo_bytes)
+        else:
+            # Write the uploaded video to the temporary file
+            tffile.write(video.read())
+            vid = open(tffile.name, 'rb')
+            vid_bytes = vid.read()
+            st.sidebar.text("Input Video")
+            st.sidebar.video(vid_bytes)
+
+        # Create an empty space to display video frames
+        stframe = st.empty()
+
+        # Create key performance indicators (KPIs) to display frame rate and license plate number
+        kpi1, kpi2, kpi3 = st.columns(3)
+        with kpi1:
+            st.markdown("**Frame Rate**")
+            kpi1_text = st.markdown("0")
+        with kpi2:
+            st.markdown("**License Plate Number**")
+            kpi2_text = st.markdown("0")
+
+        # Perform object detection and OCR on the uploaded video
+        object_detction__ocr_video(tffile.name, confidence, kpi1_text, kpi2_text, stframe)
+
+
+# Execute the main function if this script is run directly
+if __name__ == '__main__':
+    main()
---
+++ license_plate_detection.py
@@ -0,0 +1,100 @@
+# Import necessary libraries
+import cv2
+from super_gradients.training import models
+import numpy as np
+import time
+import math
+import easyocr
+import streamlit as st
+
+# Initialize EasyOCR for text recognition
+reader = easyocr.Reader(['en'], gpu=True)
+
+# Define a function for OCR on a region of an image
+def ocr_image(frame, x1, y1, x2, y2):
+    frame = frame[y1:y2, x1:x2]  # Crop the frame to the specified region
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Convert cropped region to grayscale
+    result = reader.readtext(gray)  # Perform OCR on the grayscale region
+    text = ""  # Initialize an empty string for detected text
+    for res in result:
+        if len(result) == 1:
+            text = res[1]  # If only one result, set the text to the detected text
+        if len(result) > 1 and len(res[1])>6 and res[2] >0.2:
+            text = res[1]  # If multiple results and certain conditions met, set the text to the detected text
+    return str(text)  # Return the detected text
+
+# Define a function for object detection and OCR on a single image
+def object_detection_ocr(image, confidence):
+    device = "cpu"
+    model = models.get('yolo_nas_s', 
+                        num_classes=1, 
+                        checkpoint_path='./Checkpoints/ckpt_best.pth', fuse_model=False).to(device)  # Load the object detection model
+    class_names = ['license-plate']  # Define class names (in this case, just one class)
+
+    # Perform object detection on the image
+    result = model.predict(image, conf=confidence)
+    
+    # Ensure result is not treated as an iterable if it's not structured that way
+    bbox_xyxys = result.prediction.bboxes_xyxy.tolist()  # Extract bounding box coordinates
+    confidences = result.prediction.confidence  # Extract confidence scores
+    labels = result.prediction.labels.tolist()  # Extract class labels
+    
+    # Loop through detected objects
+    for (bbox_xyxy, confidence, cls) in zip(bbox_xyxys, confidences, labels):
+        bbox = np.array(bbox_xyxy)
+        x1, y1, x2, y2 = bbox[0], bbox[1], bbox[2], bbox[3]  # Extract coordinates
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)  # Convert coordinates to integers
+        classname = int(cls)
+        class_name = class_names[classname]  # Get the class name
+        conf = math.ceil(confidence*100)/100  # Calculate rounded confidence score
+        label = f"{class_name}{conf}"  # Create label for the object
+        text = ocr_image(image, x1, y1, x2, y2)  # Perform OCR on the region of interest
+        text_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]  # Get size of label text
+        background = x1 + text_size[0], y1 - text_size[1] - 3  # Calculate background size for label
+        cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 255), 3)  # Draw bounding box
+        cv2.rectangle(image, (x1, y1), background, [255, 144, 20], -1, cv2.LINE_AA)  # Draw label background
+        cv2.putText(image, text, (x1, y1-2), 0, 1, [255, 255, 255], thickness=1, lineType=cv2.LINE_AA)  # Draw label text
+
+    # Display the output image using Streamlit
+    st.subheader('Output Image')
+    st.image(image, channels="BGR", use_container_width=True)
+
+# Define a function for object detection and OCR on a video
+def object_detction__ocr_video(video, confidence, kpi1_text, kpi2_text, stframe):
+    cap = cv2.VideoCapture(video)  # Initialize video capture from specified source
+
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+    
+    device = "cpu"
+    model = models.get('yolo_nas_s', 
+                        num_classes=1, 
+                        checkpoint_path='./Checkpoints/ckpt_best.pth', fuse_model=False).to(device)  # Load the object detection model
+    class_names = ['license-plate']  # Define class names (in this case, just one class)
+
+    prev_time = 0
+    while True:
+        ret, frame = cap.read()  # Read a frame from the video
+        if ret:
+            result = list(model.predict(frame, conf=0.4))[0]  # Perform object detection on the frame
+            bbox_xyxys = result.prediction.bboxes_xyxy.tolist()  # Extract bounding box coordinates
+            confidences = result.prediction.confidence  # Extract confidence scores
+            labels = result.prediction.labels.tolist()  # Extract class labels
+            for (bbox_xyxy, confidence, cls) in zip(bbox_xyxys, confidences, labels):
+                bbox = np.array(bbox_xyxy)
+                x1, y1, x2, y2 = bbox[0], bbox[1], bbox[2], bbox[3]  # Extract coordinates
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)  # Convert coordinates to integers
+                classname = int(cls)
+                class_name = class_names[classname]  # Get the class name
+                conf = math.ceil(confidence*100)/100  # Calculate rounded confidence score
+                label = f'{class_name}{conf}'  # Create label for the object
+                text = ocr_image(frame, x1, y1, x2, y2)  # Perform OCR on the region of interest
+                label = text  # Set label as the detected text
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 255), 3)  # Draw bounding box
+                text_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]  # Get size of label text
+                background = x1 + text_size[0], y1 - text_size[1] - 3  # Calculate background size for label
+                cv2.rectangle(frame, (x1, y1), background, [255, 144, 30], -1, cv2.LINE_AA)  # Draw label background
+                cv2.putText(frame, label, (x1, y1-2), 0, 1, [255, 255, 255], thickness=1, lineType=cv2.LINE_AA)  #
